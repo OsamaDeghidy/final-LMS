77a921dbd97cbbe77b025641705b4ad6d890ff87
Temporary User <temp@example.com>
2025-05-14 16:09:06 +0300

    2 commit

--- pilot/settings.py
+++ pilot/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-     #'website.middleware.UserActivityMiddleware',
+    # 'website.middleware.UserActivityMiddleware',
     'axes.middleware.AxesMiddleware',
     'pilot.middleware.AdminMiddleware',
     'allauth.account.middleware.AccountMiddleware',
